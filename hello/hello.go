f40019cf3183060fcd58f096d4a710316aac5e0b
namrajp <active99raj@gmail.com>
2025-08-08 11:17:46 +1000

    greeting multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -10,12 +10,15 @@ func main() {
     // Get a greeting message and print it.
     log.SetPrefix("greetings:")
     log.SetFlags(0)
+    // A slice of names.
+    names := []string{"Siwani", "Samantha", "Seetal"}
 
-    // Request the greeting message
-    message, err := greetings.Hello("Gladys")
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     if err!=nil {
 	log.Fatal(err)
     }
-    
-    fmt.Println(message)
+     // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
